ea976c99425295880aca0a04fb0ba0ad5a11e4b4
minji <minjikim@goldenplanet.co.kr>
2022-06-14 22:38:09 +0900

    [Solve] 3진법 더하기

---
+++ programmers/3진법-뒤집기.js
@@ -0,0 +1,5 @@
+// 프로그래머스 lv.1 3진법 뒤집기
+// https://programmers.co.kr/learn/courses/30/lessons/68935?language=javascript
+function solution(n) {
+  return parseInt(n.toString(3).split('').reverse().join(''), 3);
+}
